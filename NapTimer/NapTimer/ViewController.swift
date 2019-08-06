d3986d8f78c4c3cb25880fe0d5142cf4830e6b80
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-06 11:08:51 -0600

    Viewcontroller conforms to naptimer delegate

--- NapTimer/NapTimer/ViewController.swift
+++ NapTimer/NapTimer/ViewController.swift
@@ -23,16 +23,50 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        napTimer.startTimer(30)
+        napTimer.delegate = self
     }
     
     // MARK: - Actions
     
     @IBAction func timerButtonTapped(_ sender: Any) {
-        
+        if napTimer.isOn {
+            napTimer.stopTimer()
+        } else {
+            napTimer.startTimer(5)
+        }
     }
     
+    func updateLabelAndButton() {
+        timerLabel.text = napTimer.timeLeftAsString()
+        var buttonTitle = ""
+        var buttonColor: UIColor = .white
+        
+        if napTimer.isOn {
+            buttonTitle = "Stop"
+            buttonColor = .red
+        } else {
+            buttonTitle = "Start"
+            buttonColor = .green
+        }
+        
+        timerButton.setTitle(buttonTitle, for: .normal)
+        timerButton.setTitleColor(buttonColor, for: .normal)
+    }
+}
 
-
+extension ViewController: NapTimerDelegate {
+    func timerSecondTicked() {
+        updateLabelAndButton()
+    }
+    
+    func timerStopped() {
+        updateLabelAndButton()
+    }
+    
+    func timerCompleted() {
+        // Present alert
+    }
+    
+    
 }
 
